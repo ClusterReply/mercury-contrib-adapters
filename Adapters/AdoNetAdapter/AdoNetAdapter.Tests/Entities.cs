460b325a07a047a73f26af29fee6245595d72ec9
mi-tettamanti <mi.tettamanti@reply.it>
2014-04-27 20:00:09 +0200

    Inseriti test serializzazione messaggi per l'Adapter ADO.NET (da implementare)

---
+++ Adapters/AdoNetAdapter/AdoNetAdapter.Tests/Entities.cs
@@ -0,0 +1,44 @@
+﻿using Reply.Cluster.Mercury.Adapters.AdoNet.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reply.Cluster.Mercury.Adapters.AdoNet.Tests
+{
+    [DataContract(Namespace = Constants.MESSAGENAMESPACE)]
+    class Entity
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public DateTime Date { get; set; }
+
+        [DataMember]
+        public int Count { get; set; }
+
+        [DataMember]
+        public decimal Value { get; set; }
+    }
+
+    [CollectionDataContract(Name = Constants.CREATE, ItemName = Constants.ROW, Namespace = Constants.MESSAGENAMESPACE)]
+    class CreateEntity : Create<Entity> { }
+
+    [CollectionDataContract(Name = Constants.UPDATE, ItemName = Constants.ROW, Namespace = Constants.MESSAGENAMESPACE)]
+    class UpdateEntity : Update<Entity> { }
+
+    [CollectionDataContract(Name = Constants.DELETE, ItemName = Constants.ROW, Namespace = Constants.MESSAGENAMESPACE)]
+    class DeleteEntity : Delete<Entity> { }
+
+    [CollectionDataContract(Name = Constants.INBOUND_DATA, ItemName = Constants.ROW, Namespace = Constants.MESSAGENAMESPACE)]
+    class EntityInboundData : InboundData<Entity> { }
+
+    [DataContract(Name = "Procedure", Namespace = Constants.MESSAGENAMESPACE)]
+    class ExecuteEntity : Entity { }
+}
---
+++ Adapters/AdoNetAdapter/AdoNetAdapter.Tests/Messages.cs
@@ -0,0 +1,70 @@
+﻿using MbUnit.Framework;
+using Reply.Cluster.Mercury.Adapters.AdoNet.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reply.Cluster.Mercury.Adapters.AdoNet.Tests
+{
+    [TestFixture]
+    public class Messages
+    {
+        [Test]
+        public void CreateSerialize() { }
+
+        [Test]
+        public void CreateResultSerialize() { }
+
+        [Test]
+        public void ReadSerialize() { }
+
+        [Test]
+        public void UpdateSerialize() { }
+
+        [Test]
+        public void UpdateResultSerialize() { }
+
+        [Test]
+        public void DeleteSerialize() { }
+
+        [Test]
+        public void DeleteResultSerialize() { }
+
+        [Test]
+        public void ExecuteSerialize() { }
+
+        [Test]
+        public void InboundDataSerialize() { }
+
+        [Test]
+        public void CreateDeserialize() { }
+
+        [Test]
+        public void CreateResultDeserialize() { }
+
+        [Test]
+        public void ReadDeserialize() { }
+
+        [Test]
+        public void UpdateDeserialize() { }
+
+        [Test]
+        public void UpdateResultDeserialize() { }
+
+        [Test]
+        public void DeleteDeserialize() { }
+
+        [Test]
+        public void DeleteResultDeserialize() { }
+
+        [Test]
+        public void ExecuteDeserialize() { }
+
+        [Test]
+        public void InboundDataDeserialize() { }
+
+    }
+}
