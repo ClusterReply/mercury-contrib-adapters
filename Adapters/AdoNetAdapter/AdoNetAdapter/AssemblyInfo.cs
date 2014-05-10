4b51bbba2fcd25690bea3f3028fc637dfed10f55
mi-tettamanti <mi.tettamanti@reply.it>
2014-05-10 13:46:59 +0200

    Aggiunta licenza Apache all'Adapter ADO.NET

--- Adapters/AdoNetAdapter/AdoNetAdapter/AssemblyInfo.cs
+++ Adapters/AdoNetAdapter/AdoNetAdapter/AssemblyInfo.cs
@@ -1,3 +1,20 @@
+#region Copyright
+/*
+Copyright 2014 Cluster Reply s.r.l.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#endregion
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -8,12 +25,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdoNetAdapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ADO.NET Adapter")]
+[assembly: AssemblyDescription("Adapter that communicates with databases using ADO.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdoNetAdapter")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Cluster Reply s.r.l.")]
+[assembly: AssemblyProduct("Reply Mercury Adapters")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Cluster Reply s.r.l.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
