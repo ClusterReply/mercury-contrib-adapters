06bd289e6a828895ac23260c8d64abacd992c696
mi-tettamanti <mi.tettamanti@reply.it>
2014-06-02 13:38:49 +0200

    Resa astratta la base del test dell'Adapter FTP per poterne consentire la riproduzione nei test successivi

--- Adapters/FtpAdapter/FtpAdapter.Tests/FTP_DefaultPort_Anonymous_Inbound.cs
+++ Adapters/FtpAdapter/FtpAdapter.Tests/FTP_DefaultPort_Anonymous_Inbound.cs
@@ -32,247 +32,26 @@ using System.Threading.Tasks;
 namespace Reply.Cluster.Mercury.Adapters.Ftp.Tests
 {
     [TestFixture]
-    public class FTP_DefaultPort_Anonymous_Inbound
+    public class FTP_DefaultPort_Anonymous_Inbound : Inbound
     {
-        private static string directory = "FtpAdapter";
-        private static string rootFolder = Path.GetTempPath();
-        private string inputFolder = Path.Combine(rootFolder, directory);
-        private string ftpSeverPath = Path.Combine(AssemblyDirectory, "sfk171.exe");
-
-        private Process ftpServer;
-
-        private static string AssemblyDirectory
+        protected override string GetFtpServerParameters()
         {
-            get
-            {
-                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                UriBuilder uri = new UriBuilder(codeBase);
-                string path = Uri.UnescapeDataString(uri.Path);
-                return Path.GetDirectoryName(path);
-            }
+            return "ftpserv -rw";
         }
 
-        [FixtureSetUp]
-        public void StartServer()
+        protected override FtpAdapterConnectionUri GetFtpAdapterConnectionUri(string inputFolder, string filter)
         {
-            //Debugger.Launch();
-
-            var processInfo = new ProcessStartInfo(ftpSeverPath, "ftpserv -rw") { WorkingDirectory = rootFolder };
-
-            ftpServer = Process.Start(processInfo);
+            return new FtpAdapterConnectionUri { HostName = "127.0.0.1", Path = inputFolder, FileName = filter };
         }
 
-        [SetUp]
-        public void Initialize()
+        protected override string GetExpectedAction(string directory, string fileName)
         {
-            if (Directory.Exists(inputFolder))
-                Directory.Delete(inputFolder, true);
-
-            Directory.CreateDirectory(inputFolder);
+            return string.Format("ftp://127.0.0.1/{0}/{1}", directory, fileName);
         }
 
-        [Test]
-        public void Internal_ReadFile_SinglePre()
+        protected override ClientCredentials GetCredentials()
         {
-            string inputFile = Path.Combine(inputFolder, "File.txt");
-            System.IO.File.WriteAllText(inputFile, Properties.Resources.TestFile);
-
-            IConnection connection;
-            var inputHandler = Internal_CreateHandler(directory, "*.txt", out connection);
-
-            try
-            {
-                Internal_CheckFile(inputHandler, inputFile, Properties.Resources.TestFile);
-            }
-            finally
-            {
-                inputHandler.StopListener(TimeSpan.MaxValue);
-                connection.Close(TimeSpan.FromSeconds(30));
-            }
-        }
-
-        [Test]
-        public void Internal_ReadFile_SinglePost()
-        {
-            IConnection connection;
-            var inputHandler = Internal_CreateHandler(directory, "*.txt", out connection);
-
-            try
-            {
-                string inputFile = Path.Combine(inputFolder, "File.txt");
-                System.IO.File.WriteAllText(inputFile, Properties.Resources.TestFile);
-
-                Internal_CheckFile(inputHandler, inputFile, Properties.Resources.TestFile);
-            }
-            finally
-            {
-                inputHandler.StopListener(TimeSpan.MaxValue);
-                connection.Close(TimeSpan.FromSeconds(30));
-            }
-        }
-
-        [Test]
-        public void Internal_ReadFile_PreAndPost()
-        {
-            string inputFile1 = Path.Combine(inputFolder, "File1.txt");
-            System.IO.File.WriteAllText(inputFile1, Properties.Resources.TestFile);
-
-            IConnection connection;
-            var inputHandler = Internal_CreateHandler(directory, "*.txt", out connection);
-
-            try
-            {
-                Internal_CheckFile(inputHandler, inputFile1, Properties.Resources.TestFile); 
-                
-                string inputFile2 = Path.Combine(inputFolder, "File2.txt");
-                System.IO.File.WriteAllText(inputFile2, Properties.Resources.TestFile_Existing);
-
-                Internal_CheckFile(inputHandler, inputFile2, Properties.Resources.TestFile_Existing);
-            }
-            finally
-            {
-                inputHandler.StopListener(TimeSpan.MaxValue);
-                connection.Close(TimeSpan.FromSeconds(30));
-            }
-        }
-
-        [Test]
-        public void External_ReadFile_SinglePre()
-        {
-            string inputFile = Path.Combine(inputFolder, "File.txt");
-            System.IO.File.WriteAllText(inputFile, Properties.Resources.TestFile);
-
-            var serviceHost = External_CreateService(directory, "*.txt");
-
-            try
-            {
-                External_CheckFile(serviceHost, inputFile, Properties.Resources.TestFile);
-            }
-            finally
-            {
-                serviceHost.Close();
-            }
-        }
-
-        [Test]
-        public void External_ReadFile_SinglePost()
-        {
-            var serviceHost = External_CreateService(directory, "*.txt");
-
-            try
-            {
-                string inputFile = Path.Combine(inputFolder, "File.txt");
-                System.IO.File.WriteAllText(inputFile, Properties.Resources.TestFile);
-
-                External_CheckFile(serviceHost, inputFile, Properties.Resources.TestFile);
-            }
-            finally
-            {
-                serviceHost.Close();
-            }
-        }
-
-        [Test]
-        public void External_ReadFile_PreAndPost()
-        {
-            string inputFile1 = Path.Combine(inputFolder, "File1.txt");
-            System.IO.File.WriteAllText(inputFile1, Properties.Resources.TestFile);
-
-            var serviceHost = External_CreateService(directory, "*.txt");
-
-            try
-            {
-                External_CheckFile(serviceHost, inputFile1, Properties.Resources.TestFile);
-
-                string inputFile2 = Path.Combine(inputFolder, "File2.txt");
-                System.IO.File.WriteAllText(inputFile2, Properties.Resources.TestFile_Existing);
-
-                External_CheckFile(serviceHost, inputFile2, Properties.Resources.TestFile_Existing);
-            }
-            finally
-            {
-                serviceHost.Close();
-            }
-        }
-
-        private FtpAdapterInboundHandler Internal_CreateHandler(string inputFolder, string filter, out IConnection connection)
-        {
-            var adapter = new FtpAdapter { PollingInterval = 15 };
-            var connectionUri = new FtpAdapterConnectionUri { HostName = "127.0.0.1", Path = inputFolder, FileName = filter };
-            var factory = new FtpAdapterConnectionFactory(connectionUri, new ClientCredentials(), adapter);
-
-            connection = factory.CreateConnection();
-            connection.Open(TimeSpan.FromSeconds(30));
-
-            var inputHandler = connection.BuildHandler<FtpAdapterInboundHandler>(null);
-
-            inputHandler.StartListener(new string[0], TimeSpan.MaxValue);
-
-            return inputHandler;
-        }
-
-        private void Internal_CheckFile(FtpAdapterInboundHandler inputHandler, string inputFile, string expectedContent)
-        {
-            Message message;
-            IInboundReply reply;
-
-            Assert.IsTrue(inputHandler.TryReceive(TimeSpan.FromMinutes(1), out message, out reply));
-            
-            Assert.AreEqual(
-                new Uri(string.Format("ftp://127.0.0.1/{0}/{1}", directory, Path.GetFileName(inputFile))), 
-                new Uri(message.Headers.Action));
-
-            Stream body = message.GetBody<Stream>();
-
-            using (var reader = new StreamReader(body))
-                Assert.AreEqual(expectedContent, reader.ReadToEnd());
-
-            reply.Reply(message, TimeSpan.FromMinutes(1));
-
-            Assert.IsFalse(System.IO.File.Exists(inputFile));
-        }
-
-        private ServiceHost External_CreateService(string inputFolder, string filter)
-        {
-            var instance = new Service();
-            var host = new ServiceHost(instance);
-
-            var connectionUri = new FtpAdapterConnectionUri { HostName = "127.0.0.1", Path = inputFolder, FileName = filter };
-
-            host.AddServiceEndpoint(typeof(IService), new FtpAdapterBinding { PollingInterval = 15 }, connectionUri.Uri);
-            host.Open();
-
-            return host;
-        }
-
-        private void External_CheckFile(ServiceHost host, string inputFile, string expectedContent)
-        {
-            MessageItem message;
-            var instance = host.SingletonInstance as Service;
-            
-            Assert.IsTrue(instance.Queue.TryTake(out message, TimeSpan.FromMinutes(1)));
-
-            System.Threading.Thread.Sleep(1000);
-
-            Assert.AreEqual(
-                new Uri(string.Format("ftp://127.0.0.1/{0}/{1}", directory, Path.GetFileName(inputFile))), 
-                new Uri(message.Action));
-            Assert.AreEqual(expectedContent, message.Data);
-
-            Assert.IsFalse(System.IO.File.Exists(inputFile));
-        }
-
-        [TearDown]
-        public void Cleanup()
-        {
-            if (Directory.Exists(inputFolder))
-                Directory.Delete(inputFolder, true);
-        }
-
-        [FixtureTearDown]
-        public void StopServer()
-        {
-            ftpServer.Kill();
+            return new ClientCredentials();
         }
     }
 }
