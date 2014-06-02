0b4f0c135638b3c861b2fb5d169dc4f72ec86702
mi-tettamanti <mi.tettamanti@reply.it>
2014-06-02 16:15:04 +0200

    Aggiunto ClientCredentials behavior utilizzabile anche in Inbound

---
+++ Adapters/Behaviors/AdapterClientCredentials.cs
@@ -0,0 +1,42 @@
+﻿#region Copyright
+/*
+Copyright 2014 Cluster Reply s.r.l.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reply.Cluster.Mercury.Adapters.Behaviors
+{
+    public class AdapterClientCredentials : ClientCredentials, IEndpointBehavior
+    {
+        void IEndpointBehavior.AddBindingParameters(ServiceEndpoint serviceEndpoint, BindingParameterCollection bindingParameters)
+        {
+            bindingParameters.Add(this);
+        }
+
+        void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint serviceEndpoint, EndpointDispatcher endpointDispatcher)
+        { }
+
+        void IEndpointBehavior.Validate(ServiceEndpoint serviceEndpoint)
+        { }
+    }
+}
